076bc4df020e5e1ad74ac9797223fe0170bc3899
jaysonegarcia <garciajayson502@gmail.com>
2026-04-21 21:47:46 +0800

    chore: remove composer.json and vendor, rely on theme autoloader

--- results.php
+++ results.php
@@ -1,8 +1,4 @@
 <?php
-$autoload_path = __DIR__.'/vendor/autoload.php';
-if ( file_exists( $autoload_path ) ) {
-    require_once( $autoload_path );
-}
 
 $boot = new Results\Providers\ResultsServiceProvider;
 $boot->register();
